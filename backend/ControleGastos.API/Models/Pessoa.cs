5b97966e20bd0ddb6f36a00d8ed09598f03c4f76
Enzo Amaral <enzoamaral2006@gmail.com>
2026-07-28 18:27:54 -0300

    feat: add validation

--- backend/ControleGastos.API/Models/Pessoa.cs
+++ backend/ControleGastos.API/Models/Pessoa.cs
@@ -1,9 +1,15 @@
-﻿namespace ControleGastos.API.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleGastos.API.Models;
 
 public class Pessoa
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "O nome é obrigatório")]
     public string Nome { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "A idade é obrigatória")]
     public int Idade { get; set; }
     
     public List<Transacao> Transacoes { get; set; } = new();
--- backend/ControleGastos.API/Models/Transacao.cs
+++ backend/ControleGastos.API/Models/Transacao.cs
@@ -1,11 +1,16 @@
 ﻿using ControleGastos.API.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace ControleGastos.API.Models;
 
 public class Transacao
 {
     public int Id { get; set; }
+
+    [Required(ErrorMessage = "A descrição é obrigatória")]
     public string Descricao { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O valor é obrigatório")]
     public decimal Valor { get; set; }
     public TipoTransacao Tipo { get; set; }
     public int PessoaId { get; set; }
